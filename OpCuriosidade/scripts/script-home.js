02516d64250251e126d17832933b1af72d469168
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-09-04 14:59:11 -0300

    home interativa em processo

--- OpCuriosidade/scripts/script-home.js
+++ OpCuriosidade/scripts/script-home.js
@@ -1,3 +1,23 @@
 document.getElementById('toggleBt').addEventListener('click', function() {
     this.classList.toggle('active');
 });
+
+var vg = document.getElementById('vg');
+var cc = document.getElementById('cc');
+
+function cadastrarCuri(){
+    vg.style.backgroundColor = 'white';
+    vg.style.color = '#6b1bc6';
+    cc.style.backgroundColor = '#6b1bc6';
+    cc.style.color = 'white';
+    vg.onmouseover = function() {
+        vg.style.backgroundColor = '#6b1bc6';
+        vg.style.color = 'white'; // Talvez você também queira alterar a cor do texto para algo que contraste
+    };
+
+    // Quando o mouse sair de cima
+    vg.onmouseout = function() {
+        vg.style.backgroundColor = 'white';
+        vg.style.color = '#6b1bc6';
+}
+}
