4a2c70bb118eb66772e155317b7a53c35b207227
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-09-09 13:09:21 -0300

    compartilhar on

--- OpCuriosidade/scripts/script-comunicacao.js
+++ OpCuriosidade/scripts/script-comunicacao.js
@@ -1,8 +1,28 @@
 
 const compartButton = document.getElementById('compart');
+const compartButton2 = document.getElementById('compart2');
+const compartButton3 = document.getElementById('compart3');
+const compartButton4 = document.getElementById('compart4');
+const compartButton5 = document.getElementById('compart5');
 
 
 compartButton.addEventListener('click', function() {
+    window.parent.postMessage('openCompartModal', '*');
+});
+
+compartButton2.addEventListener('click', function() {
+    window.parent.postMessage('openCompartModal', '*');
+});
+
+compartButton3.addEventListener('click', function() {
+    window.parent.postMessage('openCompartModal', '*');
+});
+
+compartButton4.addEventListener('click', function() {
+    window.parent.postMessage('openCompartModal', '*');
+});
+
+compartButton5.addEventListener('click', function() {
     window.parent.postMessage('openCompartModal', '*');
 });
 
--- OpCuriosidade/scripts/script-home.js
+++ OpCuriosidade/scripts/script-home.js
@@ -147,3 +147,77 @@ window.addEventListener('message', function(event) {
 btnclosemodal.addEventListener('click', function() {
     document.querySelector('.modal').style.display = 'none';
 });
+
+var v1 = document.getElementById('v1');
+var v2 = document.getElementById('v2');
+var v3 = document.getElementById('v3');
+
+var e1 = document.getElementById('e1');
+var e2 = document.getElementById('e2');
+var e3 = document.getElementById('e3');
+
+var abrirModalzinho1 = document.getElementsByClassName('modalzinho1')[0];
+var abrirModalzinho2 = document.getElementsByClassName('modalzinho2')[0];
+var abrirModalzinho3 = document.getElementsByClassName('modalzinho3')[0];
+
+v1.addEventListener('click', function() { abrirModal(abrirModalzinho1); });
+e1.addEventListener('click', function() { abrirModal(abrirModalzinho1); });
+
+v2.addEventListener('click', function() { abrirModal(abrirModalzinho2); });
+e2.addEventListener('click', function() { abrirModal(abrirModalzinho2); });
+
+v3.addEventListener('click', function() { abrirModal(abrirModalzinho3); });
+e3.addEventListener('click', function() { abrirModal(abrirModalzinho3); });
+
+function abrirModal(modal) {
+    if (modal.style.display === 'block') {
+        modal.style.display = 'none';
+    }else{
+        modal.style.display = 'block';
+    }
+}
+
+var V1 = document.getElementById('V1');
+var E1 = document.getElementById('E1');
+
+var V2 = document.getElementById('V2');
+var E2 = document.getElementById('E2');
+
+var V3 = document.getElementById('V3');
+var E3 = document.getElementById('E3');
+
+V1.addEventListener('click', function(){
+    v1.style.display = 'block';
+    e1.style.display = 'none';
+    abrirModal(abrirModalzinho1)
+});
+
+E1.addEventListener('click', function(){
+    v1.style.display = 'none';
+    e1.style.display = 'block';
+    abrirModal(abrirModalzinho1)
+});
+
+V2.addEventListener('click', function(){
+    v2.style.display = 'block';
+    e2.style.display = 'none';
+    abrirModal(abrirModalzinho2)
+});
+
+E2.addEventListener('click', function(){
+    v2.style.display = 'none';
+    e2.style.display = 'block';
+    abrirModal(abrirModalzinho2)
+});
+
+V3.addEventListener('click', function(){
+    v3.style.display = 'block';
+    e3.style.display = 'none';
+    abrirModal(abrirModalzinho3)
+});
+
+E3.addEventListener('click', function(){
+    v3.style.display = 'none';
+    e3.style.display = 'block';
+    abrirModal(abrirModalzinho3)
+});
