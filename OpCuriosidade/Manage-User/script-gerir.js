242f4d50dfae216c389f9eedc98e762697503ace
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-10-07 14:57:03 -0300

    commit

--- OpCuriosidade/Manage-User/script-gerir.js
+++ OpCuriosidade/Manage-User/script-gerir.js
@@ -1,3 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch('https://localhost:7299/api/user-model/api/users')
+        .then(User => {
+            console.log(User);
+            if (!User.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return User.json();
+        })
+        .then(users => {
+            const userList = document.getElementById('user-list');
+            users.forEach(user => {
+                const listItem = document.createElement('li.user');
+                listItem.innerHTML = `<div class="box">
+                            <input type="checkbox" name="selecionar" id="selecionar" class="checkbox-item">
+                        </div>
+                        <div class="container-dados" id="code">
+                            <p class="codigo">24-00016</p>
+                        </div>
+                        <div class="container-dados" id="name">
+                            <p class="nome">Rosangela Maria Pereira</p>
+                        </div>
+                        <div class="container-dados" id="email">
+                            <p class="email">rosangela.pereira@fakemail.br</p>
+                        </div>
+                        <div class="container-dados" id="date">
+                            <p class="data">04/09/2024</p>
+                        </div>
+                        <div class="container-dados" id="status">
+                            <p class="status" id="statusA">Ativo</p>
+                        </div>
+                        <div class="container-dados" id="type">
+                            <p class="tipo" id="typeO">Operador</p>
+                        </div>
+                        <div class="container-dados" id="button">
+                            <p class="botao" id="btn"><span class="material-symbols-outlined">more_vert</span>Detalhes</p>
+                        </div>`;
+                userList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error('Houve um erro ao buscar os usuários:', error);
+        });
+
+});
+
 function marcarTodos(checkbox) {
     var checkboxes = document.querySelectorAll('.checkbox-item');
     
@@ -5,6 +52,8 @@ function marcarTodos(checkbox) {
         item.checked = checkbox.checked;
     });
 }
+
+
 
 const cad = document.getElementById('cadastrarUsu');
 
@@ -18,34 +67,9 @@ btn.addEventListener('click', function() {
     window.parent.postMessage('openAtivarInativarModal', '*');
 });
 
-var btnAtivo1 = document.getElementById('status');
-var btnAtivo2 = document.getElementById('ativo2');
-var btnAtivo3 = document.getElementById('ativo3');
-var btnAtivo4 = document.getElementById('ativo4');
-var btnInativo1 = document.getElementById('inativo1');
-var btnInativo2 = document.getElementById('inativo2');
+var btnStatus = document.getElementById('statusA');
 
-btnAtivo1.addEventListener('click', function() {
+btnStatus.addEventListener('click', function() {
     console.log('teste');
     window.parent.postMessage('openMCModalzinhoInativo', '*');
-});
-
-btnAtivo2.addEventListener('click', function() {
-    window.parent.postMessage('openMCModalzinhoInativo', '*');
-});
-
-btnAtivo3.addEventListener('click', function() {
-    window.parent.postMessage('openMCModalzinhoInativo', '*');
-});
-
-btnAtivo4.addEventListener('click', function() {    
-    window.parent.postMessage('openMCModalzinhoInativo', '*');
-});
-
-btnInativo1.addEventListener('click', function() {   
-    window.parent.postMessage('openMCModalzinhoAtivo', '*');
-});
-
-btnInativo2.addEventListener('click', function() {   
-    window.parent.postMessage('openMCModalzinhoAtivo', '*');
 });
