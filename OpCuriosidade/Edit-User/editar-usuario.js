43b4a7ed36c035fb5af0fb0d6567685134d9b8d6
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-10-12 19:16:56 -0300

    commit

--- OpCuriosidade/Edit-User/editar-usuario.js
+++ OpCuriosidade/Edit-User/editar-usuario.js
@@ -1,3 +1,38 @@
+let containerImg = document.getElementById('containerImg');
+let spanPerson = document.getElementById('spanPerson');
+let previousImage = null;
+
+document.getElementById('AtualizarImagem').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    containerImg.style.backgroundColor = 'transparent';
+    containerImg.style.border = '2px solid #6b1bc6';
+    spanPerson.style.display = 'none';
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const imgElement = document.getElementById('personImage');
+            imgElement.src = e.target.result;
+            imgElement.style.display = 'block';
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('removeImage').addEventListener('click', function() {
+    const imgElement = document.getElementById('personImage');
+    imgElement.src = ''; 
+    imgElement.style.display = 'none';
+    containerImg.style.backgroundColor = '#6b1bc65e';
+    containerImg.style.border = 'none';
+    spanPerson.style.display = 'block';
+
+    document.getElementById('AtualizarImagem').value = '';
+    previousImage = null;
+});
+
 document.addEventListener("DOMContentLoaded", function() {
 
     let userId = null;
@@ -33,6 +68,25 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('iTraining').value = user.facts.training;
             document.getElementById('iPhoneNumber').value = user.facts.phoneNumber;
 
+            if (user.facts.imagePath) {
+                previousImage = user.facts.imagePath;
+                document.getElementById('personImage').src = user.facts.imagePath;
+                let img = document.getElementById('personImage');
+                img.style.display = 'block';
+                let containerImg = document.getElementById('containerImg');
+                let spanPerson = document.getElementById('spanPerson');
+                containerImg.style.backgroundColor = 'transparent';
+                containerImg.style.border = '2px solid #6b1bc6';
+                spanPerson.style.display = 'none';
+            }else{
+                previousImage = null;
+                let img = document.getElementById('personImage');
+                containerImg.style.backgroundColor = '#6b1bc65e';
+                containerImg.style.border = 'none';
+                spanPerson.style.display = 'block';
+                img.style.display = 'none';
+            }
+
             document.getElementById('iType').checked = user.type === true;
             console.log('user:', user);
         })
@@ -66,47 +120,67 @@ document.addEventListener("DOMContentLoaded", function() {
         const type = document.getElementById('iType').checked;
         const forma = document.getElementById('iTraining').value;
     
-        const updatedUser = {
-            type: type,
-            password: novaSenha,
-            facts: {
-                name: name,
-                age: parseInt(age),
-                nameUser: nameUser,
-                email: email,
-                dateOfBirth: dataOfBirth,
-                maritalStatus: estadoCivil,
-                address: address,
-                profession: profissao,
-                phoneNumber: phone,
-                training: forma
-            }
-        };
+        const imageFile = document.getElementById('AtualizarImagem').files[0];
+        let imageBase64 = previousImage;
 
-        console.log(updatedUser);
-
-        fetch(`https://localhost:7299/api/user-model/up-user-id/${userId}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updatedUser),
-        })
-        .then((response) => {
-            console.log(response);
-            if (response.ok) {
-                window.alert("Usuário atualizado com sucesso");
-                location.reload();
-                closeEditarUsu();
-            } else {
-                return response.text().then((text) => {
-                    throw new Error(text);
-                });
+        if (imageFile) {
+            const reader = new FileReader();
+            reader.onloadend = function() {
+                imageBase64 = reader.result;
+                updateUser();
+            };
+            reader.readAsDataURL(imageFile);
+        } else {
+            if (!previousImage) {
+                imageBase64 = null;
             }
-        })
-        .catch((error) => {
-            console.error("Erro ao atualizar usuário:", error);
-        });
+            updateUser();
+        }
+
+        function updateUser() {
+            const updatedUser = {
+                type: type,
+                password: novaSenha,
+                facts: {
+                    name: name,
+                    age: parseInt(age),
+                    nameUser: nameUser,
+                    email: email,
+                    dateOfBirth: dataOfBirth,
+                    maritalStatus: estadoCivil,
+                    address: address,
+                    profession: profissao,
+                    phoneNumber: phone,
+                    training: forma,
+                    imagePath: imageBase64
+                }
+            };
+
+            console.log('imagemuser:', updatedUser.imageBase64);
+
+            fetch(`https://localhost:7299/api/user-model/up-user-id/${userId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(updatedUser),
+            })
+            .then((response) => {
+                console.log(response);
+                if (response.ok) {
+                    window.alert("Usuário atualizado com sucesso");
+                    location.reload();
+                    closeEditarUsu();
+                } else {
+                    return response.text().then((text) => {
+                        throw new Error(text);
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao atualizar usuário:", error);
+            });
+        }
     });
 });
 
